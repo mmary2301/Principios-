eb24f87a00dc6e4e27463aecb9ad555480736f87
mmary2301 <104003044+mmary2301@users.noreply.github.com>
2022-05-15 14:14:55 -0500

    Add files via upload

---
+++ Usuario.java
@@ -0,0 +1,12 @@
+
+public class Usuario {
+    private int saldo;
+
+    public void setSaldo(int saldo) {
+        this.saldo = this.saldo+saldo;
+    }
+
+    public int getSaldo() {
+        return saldo;
+    }
+}
